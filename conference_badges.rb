9c95e34a4325a10478a163acb320d3eb83ec57e7
Micah Banschick <micha.ban@gmail.com>
2020-01-22 17:31:20 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,9 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end 
 
-def batch_badge_creator
+def batch_badge_creator(arr)
+  new_arr = []
+  arr.each {|name| new_arr << "Hello, my name is #{name}."}
 end 
 
 def assign_rooms
